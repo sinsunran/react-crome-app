969ab5e92b7f932e3c81b71f616ae7eaf0486a98
sinsunran <115200753+sinsunran@users.noreply.github.com>
2023-01-27 23:10:50 +0900

    set dotenv and test correspond of front and back

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,11 @@
-import React, { useState } from 'react';
-const ENDPOINT = "http://localhost:4000/data";
-interface dataProps {
-  id: number
-  name: string
-  age: number
-  job: string
-}
-function App() {
-  const [data, setData] = useState<dataProps>({
-    id: 0,
-    name: "",
-    age: 0,
-    job: ""
-  });
-const getData = async (id: string) => {
-    try {
-      const res = await fetch(ENDPOINT + "/" + id)
-      const json = await res.json()
-      setData(json)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-return (
-    <div className="App">
-      <ul>
-        <li>id : {data.id}</li>
-        <li>name : {data.name}</li>
-        <li>age : {data.age}</li>
-        <li>job : {data.job}</li>
-      </ul>
-      <button onClick={() => { getData("1") }}>Data 1</button>
-      <button onClick={() => { getData("2") }}>Data 2</button>
-    </div>
-  );
+import { useState } from 'react';
+
+const App = () => {
+  return <div>
+    <form action={process.env.REACT_APP_SERVER} method="post">
+      <input type="text" />
+      <input type="submit" value="submit" />
+    </form>
+  </div>  
 }
 export default App
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
-import express, { Request, Response } from 'express'
+import "dotenv/config"
+import express ,{Request,Response}from 'express'
 import cors from "cors";
-const PORT = process.env.PORT || 4000
+import morgan from "morgan";
+
+const PORT = process.env.PORT
+const logger = morgan("dev")
 class App {
        public application: express.Application;
        constructor() {
@@ -8,24 +12,9 @@ class App {
        }
 }
 const app = new App().application;
+
 app.use(cors());
-app.get("/", (req: Request, res: Response) => {
-       res.send("server testing ok");
-})
-app.get('/data/1', (req: Request, res: Response) => {
-       res.json({
-              id: 1,
-              name: 'pepper',
-              age: 26,
-              job: 'developer'
-       })
-})
-app.get('/data/2', (req: Request, res: Response) => {
-       res.json({
-              id: 2,
-              name: 'kelly',
-              age: 21,
-              job: 'student'
-       })
-})
-app.listen(PORT, () => console.log("server running..."));             
+app.use(logger)
+app.post("/",(req:Request,res:Response)=>res.send("hi"))
+
+app.listen(PORT, () => console.log(`✅The server is now listen on port ${PORT}🚀`));          
